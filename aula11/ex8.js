1f03e875e07220f329a706d7e402acb1dd2cd97c
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-01-16 00:52:37 -0300

    add ex8

---
+++ aula11/ex8.js
@@ -0,0 +1,7 @@
+var velocidade = 78.2
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+if (velocidade > 50) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!")
+} else {
+    console.log("Dirija sempre usando cinto de segurança!")
+}
