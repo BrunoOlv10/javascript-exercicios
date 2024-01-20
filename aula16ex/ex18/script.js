08481a04607abf1f6b563ca7be71b9fd89c62153
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-01-20 19:38:27 -0300

    finish ex18

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -21,51 +21,43 @@ function inLista(num, lista) {
 
 function adicionar() {
     if (isNumero(numero.value) && !inLista(numero.value, valores)) {
-        
+        valores.push(Number(numero.value))
+        let item = document.createElement('option') 
+        item.text = `Valor ${numero.value} adicionado`
+        lista.appendChild(item)
+        resposta.innerHTML = ""
     } else {
         alert("Valor inválido ou já encontrado na lista")
     }
-
-
-//     var num = Number(numero.value)
-//     var valores = [num]
-    
-//     if (numero.value.length == 0) {
-//         alert("Digite um número")
-//     } else if (numero.value > 100 || numero.value <= 0) {
-//         alert('Digite um valor entre 1 e 100')
-//     }
-
-//     if(num == valores[num]) {
-//         alert(`O valor ${valores[num]} já está presente na lista`)
-//     } 
-
-//     if (num != valores[num]) {
-//         for(c = 0; c < numero.value.length; c++) {
-//             var item = document.createElement('option')
-//             item.text = valores
-//             lista.appendChild(item)
-//         }
-//     }
-// }
-
-// function finalizar() {
-//     for (var i = 0; i < valores[num]; i++) {
-//         var contador =+ 1
-//         if (num > valores[num]) {
-//             var maior = num
-//         }
-//         if (num < valores[num]) {
-//             var menor = num
-//         }
-//         soma += num
-//     }
-//     media = soma / i
-//     resposta.innerHTML = `Temos ao todo ${contador} números cadastrados`
-//     resposta.innerHTML = `O maior valor foi ${maior}`
-//     resposta.innerHTML = `O menor valor foi ${menor}`
-//     resposta.innerHTML = `A soma de todos os valores é ${soma}`
-//     resposta.innerHTML = `A média dos valores é ${media}`
-
+    numero.value = ''
+    numero.focus()
 }
 
+function finalizar() {
+    if(valores.length == 0) {
+        alert("Adicione valores antes de finalizar")
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let posicao in valores) {
+            soma += valores[posicao]
+            if(valores[posicao] > maior) {
+                maior = valores[posicao]
+            }
+            if (valores[posicao] < menor) {
+                menor = valores[posicao]
+            }
+        }
+        media = soma / total
+
+        resposta.innerHTML = ""
+        resposta.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
+        resposta.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        resposta.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        resposta.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        resposta.innerHTML += `<p>A média de todos os valores é ${media}</p>`
+    }
+}
