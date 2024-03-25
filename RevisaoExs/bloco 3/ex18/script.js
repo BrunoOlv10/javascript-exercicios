ee5830e5af211d9fed517ae7ab7b71ac0c832ff1
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-03-25 18:41:01 -0300

    fix bugs

--- RevisaoExs/bloco 3/ex18/script.js
+++ RevisaoExs/bloco 3/ex18/script.js
@@ -3,16 +3,16 @@ let lista = document.querySelector('select#lista');
 let resultado = document.querySelector('div#resultado');
 let valores = []
 
-function isNumero(n) {
-    if(Number(n) >= 1 && Number(n) <= 100) {
+function isNumero(num) {
+    if(Number(num) >= 1 && Number(num) <= 100) {
         return true;
     } else {
         return false;
     }
 }
 
-function inLista(n, l) {
-    if(l.indexOf(Number(n)) != -1) {
+function inLista(num, lista) {
+    if(lista.indexOf(Number(num)) != -1) {
         return true;
     } else  {
         return false;
@@ -39,11 +39,11 @@ function finalizar() {
     if(valores.length == 0) {
         alert('Nenhum número inserido!')
     } else {
-        total = valores.length
-        maior = valores[0]
-        menor = valores[0]
-        soma = 0
-        media = 0
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
 
         for(let pos in valores) {
             soma += valores[pos]
@@ -55,17 +55,19 @@ function finalizar() {
                 menor = valores[pos]
             }
         }
+        media = soma / total
         
         resultado.innerHTML = ''
-        resultado.innerHTML = `Ao todo, temos ${total} números cadastrados`
-        resultado.innerHTML = `O maior valor informado é ${maior}`
-        resultado.innerHTML = `O menor valor informado é ${menor}`
-        resultado.innerHTML = `Somando todos os valores, temos ${soma}`
-        resultado.innerHTML = `A média dos valores digitados é ${media}`
+        resultado.innerHTML += `<p>Ao todo, temos ${total} números cadastrados</p>`
+        resultado.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        resultado.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        resultado.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        resultado.innerHTML += `<p>A média de todos os valores é ${media}</p>`
     }
 }
 
 function limpar() {
+    numero.value = ''
     lista.innerHTML = ''
     resultado.innerHTML = ''
     valores = []
