c219832fd555f26226029a605395449f8f518a19
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-03-26 18:53:16 -0300

    finished ex

--- RevisaoExs/bloco 1/ex14/script.js
+++ RevisaoExs/bloco 1/ex14/script.js
@@ -1,20 +1,26 @@
-function carregar() {    
-    var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('imagem');
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas` 
-    if (hora >= 0 && hora < 12 ) {
-        // Bom dia!
+function carregar() {
+    let data = new Date() 
+    let hora = data.getHours()
+    // let hora = 21
+    let msg = document.querySelector('div#msg')
+    let img = document.querySelector('img#imagem')
+
+    msg.innerHTML = `Agora são ${hora} horas!`
+
+    if (hora >= 0 && hora < 12) {
+        msg.innerHTML += ' Bom dia!'
+        document.body.style.backgroundColor = '#e2cd9f'
         img.src = 'img/manha-redondo.png'
-        document.body.style.background = '#e2cd9f'
+        img.style.borderColor = 'goldenrod'
     } else if (hora >= 12 && hora < 18) {
-        // Boa tarde!
-        document.body.style.background = '#b9846f'
+        msg.innerHTML += ' Boa tarde!'
+        document.body.style.backgroundColor = '#b9846f'
         img.src = 'img/tarde-redondo.png'
-    } else {
-        // Boa noite!
-        img.src = 'img/noite-redondo.png'
-        document.body.style.background = '#515154'
+        img.style.borderColor = '#b9846f'
+    } else if (hora >= 18 && hora < 24) {
+        msg.innerHTML += ' Boa noite!'
+        document.body.style.backgroundColor = '#515154'
+        img.src += 'img/noite-redondo.png'
+        img.style.borderColor = '#515154'
     }
 }
--- RevisaoExs2/bloco 1/ex14/script.js
+++ RevisaoExs2/bloco 1/ex14/script.js
@@ -1,20 +1,26 @@
-let msg = document.querySelector('div#msg')
-let imagem = document.querySelector('img#imagem')
-let data = new Date()
-let hora = data.getHours()
+function carregar() {
+    let data = new Date() 
+    let hora = data.getHours()
+    // let hora = 5
+    let msg = document.querySelector('div#msg')
+    let img = document.querySelector('img#imagem')
 
-msg.innerHTML = `Agora são ${hora} horas. `
+    msg.innerHTML = `Agora são ${hora} horas!`
 
-if (hora >= 0 && hora < 12) {
-    msg.innerHTML += 'Bom dia!'
-    imagem.src = 'img/manha-redondo.png'
-    document.body.style.backgroundColor = 'goldenrod'
-} else if (hora >= 12 && hora < 18) {
-    msg.innerHTML += 'Boa tarde!'
-    imagem.src = 'img/tarde-redondo.png'
-    document.body.style.backgroundColor = 'brown'
-} else if (hora >= 18 && hora < 24) {
-    msg.innerHTML += 'Boa noite!'
-    imagem.src = 'img/noite-redondo.png'
-    document.body.style.backgroundColor = 'darkblue'
+    if (hora >= 0 && hora < 12) {
+        msg.innerHTML += ' Bom dia!'
+        document.body.style.backgroundColor = '#e2cd9f'
+        img.src = 'img/manha-redondo.png'
+        img.style.borderColor = 'goldenrod'
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML += ' Boa tarde!'
+        document.body.style.backgroundColor = '#b9846f'
+        img.src = 'img/tarde-redondo.png'
+        img.style.borderColor = '#b9846f'
+    } else if (hora >= 18 && hora < 24) {
+        msg.innerHTML += ' Boa noite!'
+        document.body.style.backgroundColor = '#515154'
+        img.src += 'img/noite-redondo.png'
+        img.style.borderColor = '#515154'
+    }
 }
