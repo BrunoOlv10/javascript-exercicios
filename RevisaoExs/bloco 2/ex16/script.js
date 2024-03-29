d00d31a5002f53d3cb6da35fd2a59dd42d334ec6
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-03-29 19:11:53 -0300

    add updates ex16

--- RevisaoExs/bloco 2/ex16/script.js
+++ RevisaoExs/bloco 2/ex16/script.js
@@ -1,32 +1,40 @@
 function contar() {
-    let inicio = document.querySelector('input#inicio')
-    let fim = document.querySelector('input#fim')
-    let passo = document.querySelector('input#passo')
-    let msg = document.querySelector('p#msg')
-    let resultado = document.querySelector('p#resultado')
+    let inicio = document.querySelector('input#inicio');
+    let fim = document.querySelector('input#fim');
+    let passo = document.querySelector('input#passo');
+    let msg = document.querySelector('p#msg');
+    let contagem = document.querySelector('p#contagem');
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('Preencha todos os campos!')
+    if (inicio.value.length == 0 && fim.value.length == 0 && passo.value.length == 0) {
+        alert('Todos os campos vazios! Preencha todos antes de continuar')
+    } else if (inicio.value.length == 0) {
+        alert('Preencha o campo "início"!')
+    } else if (fim.value.length == 0) {
+        alert('Preencha o campo "fim"!')
+    } else if (passo.value.length == 0) {
+        alert('Preencha o campo "passo"!')
     } else {
-        msg.innerHTML = 'Contando:'
+        msg.innerHTML = 'Contando: '
+        
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
         if (p <= 0) {
-            alert('Passo menor ou igual a 0. Vou considerar como 1')
+            alert('Passo menor ou igual a 0! Passo será considerado como 1')
             p = 1
-        } 
+        }
+
         if (i < f) {
             for (let c = i; c <= f; c += p) {
-                resultado.innerHTML += `${c} \u{1F449}` 
+                msg.innerHTML += `${c} \u{1F449} `
             }
         } else if (i > f) {
             for (let c = i; c >= f; c -= p) {
-                resultado.innerHTML += `${c} \u{1F449}`
+                msg.innerHTML += `${c} \u{1F449} `
             }
         }
-        resultado.innerHTML += '\u{1F3C1}'
+        msg.innerHTML += `\u{1F3C1} `
     }
 }
 
@@ -37,6 +45,6 @@ function limparCampos() {
 }
 
 function limparContagem() {
-    msg.innerHTML = 'Preparando a contagem...'
-    resultado.innerHTML = ''
+    msg.innerHTML = ''
+    contagem.innerHTML = ''
 }
--- RevisaoExs2/bloco 2/ex16/script.js
+++ RevisaoExs2/bloco 2/ex16/script.js
@@ -4,40 +4,47 @@ function contar() {
     let passo = document.querySelector('input#passo');
     let msg = document.querySelector('p#msg');
     let contagem = document.querySelector('p#contagem');
-    
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        alert('[ERRO] Preencha todos os campos!')
+
+    if (inicio.value.length == 0 && fim.value.length == 0 && passo.value.length == 0) {
+        alert('Todos os campos vazios! Preencha todos antes de continuar')
+    } else if (inicio.value.length == 0) {
+        alert('Preencha o campo "início"!')
+    } else if (fim.value.length == 0) {
+        alert('Preencha o campo "fim"!')
+    } else if (passo.value.length == 0) {
+        alert('Preencha o campo "passo"!')
     } else {
-        msg.innerHTML = 'Contando:'
+        msg.innerHTML = 'Contando: '
+        
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
         if (p <= 0) {
-            alert('[ERRO] Passo menor ou igual a 0. Vamos considerar como 1!')
+            alert('Passo menor ou igual a 0! Passo será considerado como 1')
             p = 1
         }
 
         if (i < f) {
             for (let c = i; c <= f; c += p) {
-                msg.innerHTML += `${c} \u{1F449}`
+                msg.innerHTML += `${c} \u{1F449} `
             }
         } else if (i > f) {
             for (let c = i; c >= f; c -= p) {
-                msg.innerHTML += `${c} \u{1F449}`
+                msg.innerHTML += `${c} \u{1F449} `
             }
         }
-        msg.innerHTML += '\u{1F3C1}'
+        msg.innerHTML += `\u{1F3C1} `
     }
 }
 
 function limparCampos() {
-    document.querySelector('input#inicio').value = ''
-    document.querySelector('input#fim').value = ''
-    document.querySelector('input#passo').value = ''
+    inicio.value = ''
+    fim.value = ''
+    passo.value = ''
 }
 
 function limparContagem() {
-    msg.innerHTML = 'Preparando a contagem...'
+    msg.innerHTML = ''
     contagem.innerHTML = ''
 }
