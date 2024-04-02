50e7ee04d72552cd90ce77db485488a5f5437906
Bruno Oliveira <91139220+BrunoOlv10@users.noreply.github.com>
2024-04-02 16:23:54 -0300

    Update script.js

--- RevisaoExs2/bloco 2/ex17/script.js
+++ RevisaoExs2/bloco 2/ex17/script.js
@@ -1,12 +1,12 @@
 function gerarTabuada() {
-    let numero = document.querySelector('input#numero')
-    let tabuada = document.querySelector('select#tabuada')
+    let numero = document.querySelector('input#numero');
+    let tabuada = document.querySelector('select#tabuada');
 
     if (numero.value.length == 0) {
         alert ('Nenhum número digitado!')
         numero.focus()
     } else if (numero.value <= 0) {
-        alert ('Número digitado menor ou igual a 0!') 
+        alert ('Número digitado menor ou igual a 0!')
         numero.focus()
     } else {
         let num = Number(numero.value)
